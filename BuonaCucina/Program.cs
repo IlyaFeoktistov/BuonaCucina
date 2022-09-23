e5ff1588e71eb846c050a7d9c78cbb873dad26f5
Ilya Feoktistov <ffuull123@gmail.com>
2022-09-23 17:45:36 +0300

    Initial MVC

--- BuonaCucina/Program.cs
+++ BuonaCucina/Program.cs
@@ -5,9 +5,14 @@ namespace BuonaCucina
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddMvc();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.MapControllerRoute(
+                name: "Default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
